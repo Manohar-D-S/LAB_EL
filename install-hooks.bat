d51710dfb52642985125a3e578d4c036a3fa4919
tildethinker <manojt97420@gmail.com>
2025-05-17 23:49:40 +0530

    Unified post-merge hook to install Node and Python dependencies

--- install-hooks.bat
+++ install-hooks.bat
@@ -1,4 +1,6 @@
 @echo off
-echo [Hook Installer] Copying git hooks into .git/hooks...
+echo [Hook Installer] Copying git-hooks to .git/hooks...
+
 xcopy /Y /Q git-hooks\* .git\hooks\
-echo [Hook Installer] Done.
+
+echo [Hook Installer] Done. Hook installed successfully!
