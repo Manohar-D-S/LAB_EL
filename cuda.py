66304612dda0e9aecd62a7a0e558e252227f77e9
Manjunath S <mohan24005@gmail.com>
2025-07-02 01:39:29 +0530

    added model to github

--- cuda.py
+++
@@ -1,4 +0,0 @@
-import torch
-print("CUDA Available:", torch.cuda.is_available())
-print("cuDNN Version:", torch.backends.cudnn.version())
-print("GPU:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "Not available")
